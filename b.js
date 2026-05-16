fb07a996476b187fb45ebbf77256e13866f3d036
JimmyNeutronsSon <ihtiram4396@gmail.com>
2026-05-15 20:45:19 -0700

    Fix script injection syntax in b.js

--- b.js
+++ b.js
@@ -1,1 +1,1 @@
-javascript:(function(){const s=document.createElement("script");s.src="https://cdn.jsdelivr.net/gh/JimmyNeutronsSon/JimmyNeutronsSon.github.io@main/iboss.js";document.body.appendChild(s);})();
+(function(){const s=document.createElement("script");s.src="https://cdn.jsdelivr.net/gh/JimmyNeutronsSon/JimmyNeutronsSon.github.io@main/iboss.js";document.body.appendChild(s);})();
